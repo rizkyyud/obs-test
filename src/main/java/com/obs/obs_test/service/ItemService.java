8f3a8bf69cc55e89ee8b8512daefc7092198d9c7
Rizky Ramadhan <Rizkyyud27@gmail.com>
2024-06-19 16:50:49 +0700

    perbaikan delete item

--- src/main/java/com/obs/obs_test/service/ItemService.java
+++ src/main/java/com/obs/obs_test/service/ItemService.java
@@ -1,7 +1,5 @@
 package com.obs.obs_test.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
--- src/main/java/com/obs/obs_test/usecase/Item/DeleteItemUseCase.java
+++ src/main/java/com/obs/obs_test/usecase/Item/DeleteItemUseCase.java
@@ -1,8 +1,12 @@
 package com.obs.obs_test.usecase.Item;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.obs.obs_test.exception.BadRequestException;
+import com.obs.obs_test.model.entity.Item;
 import com.obs.obs_test.repository.ItemRepository;
 
 @Component
@@ -12,6 +16,13 @@ public class DeleteItemUseCase {
     private ItemRepository itemRepository;
 
     public void execute(Long id) {
-        itemRepository.deleteById(id);
+        Optional<Item> item = itemRepository.findById(id);
+
+        if (item.isPresent()) {
+            itemRepository.deleteById(id);
+        } else {
+            throw new BadRequestException("Item id not found");
+        }
+
     }
 }
